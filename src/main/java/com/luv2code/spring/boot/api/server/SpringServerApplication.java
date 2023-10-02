19ec98d07d3ca47b3d9ce066bb82ea62ccbb17f4
gahlawat80 <gahlawat80@hotmail.com>
2023-10-02 15:10:12 -0400

    initial commit

---
+++ src/main/java/com/luv2code/spring/boot/api/server/SpringServerApplication.java
@@ -0,0 +1,23 @@
+package com.luv2code.spring.boot.api.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringServerApplication {
+	int count;
+	@GetMapping("/greet")
+	public String greet(){
+		count++;
+		return "Welcome to spring boot app deployment to AWS!!!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringServerApplication.class, args);
+	}
+
+}
